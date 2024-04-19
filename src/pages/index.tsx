76d4a5453f26cdcfcc157f1b335eb00ad6aaf701
tiyo5151 <s1F102400224@iniad.org>
2024-04-19 21:09:13 +0900

    Welcome を　8888888888888888888888888888888　にかえた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          8888888888888888888888888888888 to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
