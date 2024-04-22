92c8423093c0a72f8742dfa35f8cf00e992f4246
tiyo5151 <s1F102400224@iniad.org>
2024-04-23 04:23:59 +0900

    少し進んだ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,56 +2,58 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const directions = [
-  [1,0],
-  [1,1],
-  [0,1],
-  [1,-1],
-  [-1,0],
-  [-1,-1],
-  [0,-1],
-  [-1,1],
-]
-
-
+  [1, 0],
+  [1, 1],
+  [0, 1],
+  [1, -1],
+  [-1, 0],
+  [-1, -1],
+  [0, -1],
+  [-1, 1],
+];
 
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 0],
+    [0, 0, 0, 0, 0, 1, 2, 0],
+    [0, 0, 0, 0, 2, 1, 2, 0],
+    [0, 0, 0, 1, 2, 1, 2, 0],
+    [0, 0, 2, 1, 2, 1, 2, 0],
+    [0, 1, 2, 1, 2, 1, 2, 0],
+    [2, 1, 2, 1, 2, 1, 2, 0],
+    [1, 2, 1, 2, 1, 2, 1, 0],
   ]);
+
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
-for (const direction of directions)
-  while (turnColor !== 0,undefined)
-  if (setTurnColor === 1)
-    if (turnColor === 1){break;}
-  }else{
-    if (turnColor === 2){break;}
-      if ()
-
-
-
-
+    let n = 0;
+    let m = 0;
+    if (board[y][x] !== 0) return;
     const newBoard = structuredClone(board);
-    if (board[y + 1][x] !== undefined && board[y + 1][x] === 3 - turnColor) {
-      setTurnColor(3 - turnColor);
+    while (n < 6) {
+      n = n + 1;
+      if (board[y + n][x] !== undefined && board[y + n][x] === 3 - turnColor) {
+        if (
+          board[y + n + 1][x] !== undefined &&
+          board[y + n + 1][x] === turnColor &&
+          board[y + 1][x] !== 0
+        ) {
+          newBoard[y][x] = turnColor;
+          while (n + 1 > m) {
+            newBoard[y + m][x] = turnColor;
+            setTurnColor(3 - turnColor);
+            if (board[y + m + 1][x] === turnColor) {
+              break;
+            }
+            m = m + 1;
+          }
+        }
+      }
     }
-
-    newBoard[y][x] = turnColor;
-    setTurnColor(3 - turnColor);
-
-    newBoard[y][x] = turnColor;
-
-    setTurnColor(turnColor === 1 ? 2 : 1);
     setBoard(newBoard);
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.board}>
@@ -71,5 +73,4 @@ for (const direction of directions)
     </div>
   );
 };
-
 export default Home;
